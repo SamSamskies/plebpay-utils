22f7a2529dc37f9a61b137db1e8e29e67696faec
Sam Samskies <samsamskies@gmail.com>
2022-01-24 23:10:44 -0600

    add fp fallback for when document.referrer doesn't work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const FingerprintJS = require("@fingerprintjs/fingerprintjs");
+const fpPromise = FingerprintJS.load({ monitoring: false });
+
 const testLocalStorage = () => {
   const test = "plebPayTest";
 
@@ -10,7 +13,17 @@ const testLocalStorage = () => {
   }
 };
 
-const redirectIfNecessary = (plebPayUrl) => {
+const checkPlebPayRef = () => {
+  return fpPromise
+    .then((fp) => fp.get())
+    .then(({ visitorId }) => {
+      const searchParams = new URLSearchParams(window.location.search);
+
+      return visitorId === searchParams.get("plebPayRef");
+    });
+};
+
+const redirectIfNecessary = async (plebPayUrl) => {
   const localStorageIsAvailable = testLocalStorage();
 
   if (localStorageIsAvailable && localStorage.getItem(plebPayUrl, "1")) {
@@ -18,6 +31,19 @@ const redirectIfNecessary = (plebPayUrl) => {
   }
 
   if (document.referrer === "https://www.plebpay.com/") {
+    if (localStorageIsAvailable) {
+      localStorage.setItem(plebPayUrl, "1");
+    }
+
+    return;
+  }
+
+  if (await checkPlebPayRef()) {
+    const currentHref = new URL(window.location.href);
+
+    currentHref.searchParams.delete("plebPayRef");
+    window.history.replaceState(null, null, currentHref);
+
     if (localStorageIsAvailable) {
       localStorage.setItem(plebPayUrl, "1");
     }
