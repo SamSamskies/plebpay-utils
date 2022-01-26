bab991014b5e81525dbbb9999a9755e7809e63b3
Sam Samskies <samsamskies@gmail.com>
2022-01-25 22:10:45 -0600

    all fallback for when document.referrer isn't available

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import shortHash from "shorthash2";
+
+const getPlebPayRefFromQueryParams = () => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  return searchParams.get("plebPayRef");
+};
+
+const createLocalPlebPlayRef = (invoiceId) => {
+  return shortHash(`${invoiceId}${window.navigator.userAgent}`);
+};
+
+const removePlebPayRefFromQueryParams = () => {
+  const currentHref = new URL(window.location.href);
+
+  currentHref.searchParams.delete("plebPayRef");
+  window.history.replaceState(null, null, currentHref);
+};
+
+const getPlebPayInvoiceId = (plebPlayUrl) => {
+  return plebPlayUrl.replace("https://www.plebpay.com/", "");
+};
+
 const testLocalStorage = () => {
   const test = "plebPayTest";
 
@@ -10,16 +33,26 @@ const testLocalStorage = () => {
   }
 };
 
+const handleSuccess = (plebPlayRef) => {
+  localStorage.setItem(plebPlayRef, "1");
+  removePlebPayRefFromQueryParams();
+};
+
 const redirectIfNecessary = (plebPayUrl) => {
   const localStorageIsAvailable = testLocalStorage();
+  const invoiceId = getPlebPayInvoiceId(plebPayUrl);
+  const plebPlayRef = createLocalPlebPlayRef(invoiceId);
 
-  if (localStorageIsAvailable && localStorage.getItem(plebPayUrl, "1")) {
+  if (localStorageIsAvailable && localStorage.getItem(plebPlayRef, "1")) {
     return;
   }
 
-  if (document.referrer === "https://www.plebpay.com/") {
+  if (
+    document.referrer === "https://www.plebpay.com/" ||
+    plebPlayRef === getPlebPayRefFromQueryParams()
+  ) {
     if (localStorageIsAvailable) {
-      localStorage.setItem(plebPayUrl, "1");
+      handleSuccess(plebPlayRef);
     }
 
     return;
